03a295df8d9ac2dacc081277fabf9f06f3dfdd95
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-09-28 00:27:58 +0100

    Refactor passport.js to use async/await.

--- services/passport.js
+++ services/passport.js
@@ -22,19 +22,14 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-      User.findOne({googleId: profile.id})
-        .then(existingUser => {
-          if(existingUser) {
-            // user already exists
-            done(null, existingUser);
-          }else {
-            // user doesn't exitst, make a ew record
-            new User({googleId: profile.id})
-              .save()
-              .then(user => done(null, user));
-          }
-        });
+  }, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+    const user = await new User({ googleId: profile.id }).save();
+    done(null, user);
+
   })
 );
 
